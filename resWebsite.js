2405ecda4141d19ca3a8a47e77da8efd4d8a5255
Abdellah <smahiabdellahv&@gmail.com>
2025-04-10 16:46:35 +0100

    create Responsive Website

---
+++ resWebsite.js
@@ -0,0 +1,55 @@
+let menuHamIcone = document.querySelector(".menu-ham-icone"),
+  menuHum = document.querySelector(".menu-ham"),
+  navItem = document.querySelectorAll(".nav-item a");
+header = document.querySelector("header");
+
+/* header part */
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 590) {
+    header.classList.add("forHeader");
+    let i;
+    for (i = 0; i < navItem.length; i++) {
+      navItem[i].classList.add("nav-item-add");
+      console.log(navItem[i]);
+    }
+  } else {
+    header.classList.remove("forHeader");
+    for (i = 0; i < navItem.length; i++) {
+      navItem[i].classList.remove("nav-item-add");
+      console.log(navItem[i]);
+    }
+  }
+});
+
+/* menu icon controll */
+let help = false;
+menuHamIcone.addEventListener("click", function () {
+  if (help == false) {
+    menuHamIcone.innerHTML = '<i class="ri-close-large-line"></i>';
+    menuHum.classList.add("show");
+    help = true;
+  } else if (help == true) {
+    menuHamIcone.innerHTML = ' <i class="ri-menu-line"></i>';
+    menuHum.classList.remove("show");
+    help = false;
+  }
+});
+addEventListener("resize", () => {
+  if (window.innerWidth >= 1150) {
+    menuHamIcone.innerHTML = ' <i class="ri-menu-line"></i>';
+    menuHum.classList.remove("show");
+    help = false;
+  }
+});
+
+/* shop now */
+let threeProducts = document.querySelectorAll(".three-products li img"),
+  bigProductImg = document.getElementById("big-img"),
+  productName = document.querySelector(".product-name");
+
+threeProducts.forEach((a) =>
+  a.addEventListener("click", function () {
+    productName.innerHTML = a.name;
+    bigProductImg.src = a.src;
+  })
+);
